dafffa2af42eed82d5127928eb2f7d01035d0744
millelirio <mile.lirio@gmail.com>
2022-05-10 01:02:16 -0300

    Questão da aula 2

---
+++ aula02/CadastroJogador.java
@@ -0,0 +1,30 @@
+package aula02;
+
+import java.time.LocalDate;
+import java.time.chrono.ChronoLocalDate;
+
+public class CadastroJogador {
+    public static void main (String [] args){
+    Jogador[] listaJogadores = new Jogador[11];
+    listaJogadores[0] = new Jogador(1,"Washignton Sebastian", "Loco Abreu", LocalDate.of(1976,11,17), 1, "Atacante", 90,0,1);
+    listaJogadores[1] = new Jogador(2,"Neymar", "Menino Ney", LocalDate.of(1992,2,5), 10, "Atacante", 40,2,0);
+    listaJogadores[2] = new Jogador(3,"Vinicus Junior", "Viny", LocalDate.of(2000,7,12), 18, "Atacante", 80,0,0);
+    listaJogadores[3] = new Jogador(4,"Phillipe Coutinho", "Phil", LocalDate.of(1992,6,12), 23, "Meia", 85,1,0);
+    listaJogadores[4] = new Jogador(5,"Alisson Becker", "Ali", LocalDate.of(1992,10,2), 1, "Goleiro", 95,0,0);
+    listaJogadores[5] = new Jogador(6,"Daniel Alves", "Dan", LocalDate.of(1983,5,6), 8, "Defensor", 55,1,1);
+    listaJogadores[6] = new Jogador(7,"Lucas Paquetá", "Paquetá", LocalDate.of(1997,8,27), 17, "Meia", 85,1,0);
+    listaJogadores[7] = new Jogador(8,"Gilvanildo Vieira", "Hulk", LocalDate.of(1986,7,25), 7, "Atacante", 70,0,0);
+    listaJogadores[8] = new Jogador(9,"Roberto Firmino", "Firmino", LocalDate.of(1991,10,2), 9, "Atacante", 40,2,0);
+    listaJogadores[9] = new Jogador(10,"Claudio Luis", "Claudinho", LocalDate.of(1997,1,28), 11, "Meia", 40,2,0);
+    listaJogadores[10] = new Jogador(11,"Matheus Nunes", "Nunes", LocalDate.of(1998,8,17), 7, "Meia", 45,2,0);
+
+    listaJogadores[10].aplicarCartaoVermelho();
+    listaJogadores[8].aplicarCartaoAmarelo(2);
+    listaJogadores[10].cumprirSuspencao();
+    listaJogadores[10].sofrerLesao();
+        for(int i =0; i<11 ; i++){
+        System.out.println(listaJogadores[i]);
+    }
+        System.out.println("Depois da lesão o jogador " + listaJogadores[10].nome + " esta com a qualidade de: " + listaJogadores[10].qualidade);
+}
+}
---
+++ aula02/Jogador.java
@@ -0,0 +1,82 @@
+package aula02;
+
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.Random;
+
+
+class Jogador {
+
+    int id;
+    String nome;
+    String apelido;
+    LocalDate dataNascimento;
+    int numero;
+    String posicao;
+    double qualidade;
+    int cartoesAmarelos;
+    int cartaoVermelho;
+    Boolean suspenso;
+
+    void Jogador() {
+
+    }
+
+    public Jogador(int id, String nome, String apelido, LocalDate dataNascimento, int numero, String posicao, int qualidade, int cartoesAmarelos, int cartaoVermelho) {
+        this.id = id;
+        this.nome = nome;
+        this.apelido = apelido;
+        this.dataNascimento = dataNascimento;
+        this.numero = numero;
+        this.posicao = posicao;
+        this.qualidade = qualidade;
+        this.cartoesAmarelos = cartoesAmarelos;
+        this.cartaoVermelho = cartaoVermelho;
+
+    }
+        boolean verficicarJogo(){
+            if(cartoesAmarelos >=3 || cartaoVermelho == 1) {
+                return suspenso = true;
+            }
+                return suspenso = false;
+        }
+
+        String transforDataParaString(){
+            DateTimeFormatter formato = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+            String dataFormatada = formato.format(this.dataNascimento);
+            return dataFormatada;
+        }
+        @Override
+        public String toString(){
+        verficicarJogo();
+        if(suspenso){
+            return this.posicao +": " + this.id +" - " + this.nome + "(" + this.apelido+ ") " + transforDataParaString() + " CONDIÇÃO: NÃO PODE JOGAR";
+        } else {
+            return this.posicao +": " + this.id +" - " + this.nome + "(" + this.apelido+ ") " + transforDataParaString()+ " CONDIÇÃO: PODE JOGAR";
+        }
+        }
+        void aplicarCartaoAmarelo(int quantidade){
+            this.cartoesAmarelos =+ quantidade;
+        }
+        void aplicarCartaoVermelho(){
+            this.cartaoVermelho = 1;
+        }
+        void cumprirSuspencao(){
+            this.cartaoVermelho = 0;
+            this.cartoesAmarelos = 0;
+        }
+        void sofrerLesao(){
+            Random random = new Random();
+
+            int lesao = random.nextInt(100);
+            if(lesao <= 5) this.qualidade = this. qualidade - this.qualidade * 0.15;
+            else if(lesao <=10) this.qualidade = this. qualidade - this.qualidade * 0.10;
+            else if(lesao <=15) this.qualidade = this. qualidade -this.qualidade * 0.05;
+            else if(lesao <=30) {
+                this.qualidade = this. qualidade -2;
+            } else this.qualidade = this. qualidade -1;
+        }
+
+
+}
+
